148bc15ca972f588f0c28145211a2c1dc383be32
cosmic_wavelet <11345580-cosmic_wavelet@users.noreply.gitlab.com>
2022-11-29 13:45:35 +0530

    final

---
+++ backend/config/db.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose'
+
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect('mongodb+srv://abc:abc@cluster0.tw3gzau.mongodb.net/test', {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    })
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline)
+  } catch (error) {
+    console.error(`Error: ${error.message}`.red.underline.bold)
+    process.exit(1)
+  }
+}
+
+export default connectDB
---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    name: 'Strawhat',
+    image: '/images/strawhat.jpeg',
+    description:
+      'A straw hat is a wide-brimmed hat woven out of straw or straw-like synthetic materials. Straw hats are a type of sun hat designed to shade the head and face from direct sunlight, but are also used in fashion as a decorative element or a uniform. ',
+    brand: 'Luffy Kumar',
+    category: 'Apparel',
+    price: 4.99,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Painted earthen pot',
+    image: '/images/mtke.jpeg',
+    description:
+      'They are made by the combination of two types of mud clay: the first is taken from the surface of the earth and the second after digging more than 10 feet deeper into the earth. ',
+    brand: 'Surya',
+    category: 'decoration',
+    price: 19.99,
+    countInStock: 10,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Bronze elephant',
+    image: '/images/bronze-elephant.jpeg',
+    description:
+      ' Made of the high-quality polystone, Elephant Figurines Home Decor is durable and fadeless.Perfect size for room decor: Height 9.5 in. (24 cm), Length 6.7 in. (17 cm).Color: Bronze ',
+    brand: 'Vijay',
+    category: 'Decor',
+    price: 99.99,
+    countInStock: 5,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Straw Hand Fan',
+    image: '/images/strawhandfan.jpg',
+    description:
+      'A handheld fan, or simply hand fan, is any broad, flat surface that is waved back-and-forth to create an airflow. Generally, purpose-made handheld fans are folding fans, which are shaped like a sector of a circle and made of a thin material (such as paper or feathers) mounted on slats which revolve around a pivot so that it can be closed when not in use. Hand fans were used before mechanical fans were invented.',
+    brand: 'Gopi',
+    category: 'Decoration',
+    price: 9.99,
+    countInStock: 20,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Earthen Lamp',
+    image: '/images/earthenlamp.jpeg',
+    description:
+      'Clay diyas are symbolically lit during prayers, rituals and ceremonies; they are permanent fixtures in homes and temples. The warm, bright glow emitted from a diya is considered auspicious - it represents enlightenment, prosperity, knowledge and wisdom. Diyas represent the triumph of light over dark, good over evil with the most notable example of this being on the day of Diwali. ',
+    brand: 'Karen',
+    category: 'Decoration',
+    price: 4.99,
+    countInStock: 500,
+    rating: 0,
+    numReviews: 0,
+  },
+  {
+    name: 'Straw fan',
+    image: '/images/strawfan.jpeg',
+    description:
+      'Straw fans are used in extreme heat to relieve ourseleves of heat in a traditional way',
+    brand: 'Krishna',
+    category: 'Decoration',
+    price: 7.33,
+    countInStock: 50,
+    rating: 0,
+    numReviews: 0,
+  }
+]
+
+export default products
